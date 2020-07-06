e79bc65b652015baaa160631a964d4b4d86fc6c4
ciurkoj <jakub1798@gmail.com>
2020-07-06 21:24:50 +0100

    Added basic, functional GUI
    App displays a map, with slider user can switch between different hours
    during a day.

--- VisualisationExample.m
+++ VisualisationExample.m
@@ -1,16 +1,16 @@
 %%Create some data
-a = ncinfo('o3_surface_20180701000000.nc');
+a = ncinfo('Data/Model Combined/o3_surface_20180701000000.nc');
 chimere_ozone = {a.Variables.Name};
-table = ncread('o3_surface_20180701000000.nc',chimere_ozone{1});
+table = ncread('Data/Model Combined/o3_surface_20180701000000.nc',chimere_ozone{1});
 size(table);
-[X] = ncread('o3_surface_20180701000000.nc', 'lat')'; % create X value
-[Y] = ncread('o3_surface_20180701000000.nc', 'lon')';% create Y values
+[X] = ncread('Data/Model Combined/o3_surface_20180701000000.nc', 'lat')'; % create X value
+[Y] = ncread('Data/Model Combined/o3_surface_20180701000000.nc', 'lon')';% create Y values
 % create a mesh of valuesS
 [Y] = Y(1:698);
 [X] = X(1:398);
 X = double(X);
 Y = double(Y)';
-Z = readtable('24Hour/24HR_Orig_01.csv');
+Z = readtable('Data/24Hour/24HR_Orig_01.csv');
 Z = table2array(Z);
 Z=Z';
 [X,Y] = meshgrid(X, Y);
@@ -31,7 +31,6 @@ geoshow(cities, 'Marker', '.', 'Color', 'red')
 v = VideoWriter('plot.avi');
 v.FrameRate = 4;
 open(v);
-fileDirectory = dir('24Hour/24HR_CBE_*.csv');
 for k = 1 : length(fileDirectory)
     worldmap('Europe'); % set the part of the earth to show
     load coastlines
@@ -56,33 +55,9 @@ for k = 1 : length(fileDirectory)
     cla
     pause(0.05);
 end
+close all;
+
 close(v);
-
-% create the map
-worldmap('Europe'); % set the part of the earth to show
-load coastlines
-plotm(coastlat,coastlon)
-
-land = shaperead('landareas', 'UseGeoCoords', true);
-geoshow(gca, land, 'FaceColor', [0.5 0.7 0.5])
-
-lakes = shaperead('worldlakes', 'UseGeoCoords', true);
-geoshow(lakes, 'FaceColor', 'blue')
-
-rivers = shaperead('worldrivers', 'UseGeoCoords', true);
-geoshow(rivers, 'Color', 'blue')
-
-cities = shaperead('worldcities', 'UseGeoCoords', true);
-geoshow(cities, 'Marker', '.', 'Color', 'red')
-
-% display the data
-NumContours = 5;
-contourfm(X, Y, Z, NumContours, 'Linewidth', 0.1);
-
-% This is a bit advanced, sets the visibility of the various parts of the
-% plot so the land, cities etc shows through.
-Plots = findobj(gca,'Type','Axes');
-Plots.SortMethod = 'depth';
 
 
 
